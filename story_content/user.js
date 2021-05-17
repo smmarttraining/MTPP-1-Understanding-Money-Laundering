37cb3dc7eec36f1ef94daad356955e9a9191b51f
aoropio <andrewmark.a.oropio@smretail.com>
2021-05-17 15:40:18 +0800

    MTTP-2

--- story_content/user.js
+++ story_content/user.js
@@ -2,20 +2,14 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6rSsrFc1EZ3":
+      case "5p4xs8Py31m":
         Script1();
         break;
-      case "6hCmt9rRCjn":
+      case "691XUKgBaaE":
         Script2();
         break;
-      case "5vgr6gQvPJO":
+      case "5hXY2rTuBJh":
         Script3();
-        break;
-      case "5tohBPI2tT5":
-        Script4();
-        break;
-      case "5pDb1vXP9IT":
-        Script5();
         break;
   }
 }
@@ -38,7 +32,7 @@ function add_line() {
 if(document.getElementById('bgSong')==null){
 	add_line();
 var audio = document.getElementById('bgSong');
-audio.volume = 0.03;
+audio.volume = 0.02;
 }
 
 }
@@ -56,34 +50,24 @@ audio.play();
 
 function Script3()
 {
-  var player = GetPlayer();
-this.Location= player.GetVar("location");
-
-var audio = document.getElementById('bgSong');
-audio.src=Location + "1.mp3";
-audio.load();
-audio.play();
+  //load the scripts dynamically into the head of the document
+function add_line() {
+    var line = document.createElement("audio");
+    var head=document.getElementsByTagName('body')[0];
+    line.type = "audio/mp3";
+    line.src="";
+    line.id="bgSong" ;
+	line.autoplay = true;
+	line.loop = true;
+    head.appendChild(line);
 }
 
-function Script4()
-{
-  var player = GetPlayer();
-this.Location= player.GetVar("location");
-
+//but we only want to add these once!
+if(document.getElementById('bgSong')==null){
+	add_line();
 var audio = document.getElementById('bgSong');
-audio.src=Location + "1.mp3";
-audio.load();
-audio.play();
+audio.volume = 0.03;
 }
 
-function Script5()
-{
-  var player = GetPlayer();
-this.Location= player.GetVar("location");
-
-var audio = document.getElementById('bgSong');
-audio.src=Location + "1.mp3";
-audio.load();
-audio.play();
 }
 
